3474d731194d5249acfb8161a65e944d34c4820d
vic <vn.deguzman@gmail.com>
2017-11-20 07:09:31 +0800

    Added boilerplate for app.js. Added bootstrap to pacakages.

---
+++ static/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <h2>Counter App</h2>
+        </div>
+        <p className="app-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
